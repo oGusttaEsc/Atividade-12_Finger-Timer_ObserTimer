1029773e39239fd47fbd37535bb001306538526a
oGusttaEsc <gustavo_m_silvestre@estudante.sesisenai.org.br>
2025-05-05 07:46:26 -0300

    UPDATE - atualizando o js com os valores e funcoes

---
+++ script.js
@@ -0,0 +1,19 @@
+let startTime, running = false;
+let times = [];
+let scrambles = [];
+
+const timer = document.getElementById('timer');
+const delta = document.getElementById('delta');
+const scrambleDiv = document.getElementById('scramble');
+const ao5 = document.getElementById('ao5');
+const ao12 = document.getElementById('ao12');
+const history = document.getElementById('history');
+const toggleHistory = document.getElementById('toggleHistory');
+
+function generateScramble()
+
+function updateTime()
+
+function calcAverage(arr)
+
+function updateHistory()
